38c9140ea7a8c778f49f3acb0f47226669342702
Nyahende <michaelnyahende8@gmail.com>
2022-02-28 01:30:48 -0800

    fifth commit

--- routes/web.php
+++ routes/web.php
@@ -35,13 +35,7 @@ use App\Http\Controllers\transportController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
 
